59e3cada657aa7d3bf521a5f136ced1320adf1c6
LOWKOO2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-07-31 01:59:30 -0400

    Update main-support.js

--- plugins/main-support.js
+++ plugins/main-support.js
@@ -2,35 +2,37 @@
 let handler = async (m, { conn }) => {
 
 m.reply(`
-≡  *${botName}ᴮᴼᵀ ┃ SUPPORT*
+≡  *${botName} 🤖┃ ᴮᴼᵀ GROUP SUPPORT*
 
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ Canal
-${fgcanal}
+▢ ┃ 𒀯  Ⲙ  ⵡ  Ⲡ  Ɗ  Ⲟ  𒂶 𐓈𝛯  𐤠Ƿ𝙊☈Ʈ𝛯Ⲋ   ➊➑⁺ 💬  𒀯 ┃
+https://chat.whatsapp.com/Ke1Aghdz0QVII6kyXSQv7E 
 
-▢ Grupo *1*
-${bgp}
+▢ ┃𒀭⛓️𝜩ⲚᏝ𐤠Ꮸ𝜩Ⲋ 𒂶 Ꭾ𝜩Ꮭ༏ᏨปᏝ𐤠Ⲋ ⁿᵉᵗᶠˡⁱˣ ᵒⁿˡⁱⁿᵉ 🍿 𒀭┃
+https://chat.whatsapp.com/HvzT09WBkuG9DIa7e4zMuE
 
-▢ Grupo *2*
-${bgp2}
+▢ ┃🖲️⃢🎮 ꤈𝜩Ƭ'𝚂 Ƿ꤈𐤠ⲩ 𒂶 ᒍꪙ𝜩ᏵⲞⲊ 𐤠ᴾᴷˢ ꖾᴬᶜᴷˢ 📲⃢🍿┃
+https://chat.whatsapp.com/Hq2bBBAg3bpBDo1Zrpfw3Y
 
-▢ Grupo *NSFW* 🔞
-${bgp3}
+▢ ┃✨ 🎧⃤ ⏪⏸⏩ Ⲙ 𞤋 Ꚃ ༏ ꓚ 𐤠 Ꚃ  𒂶  ꪜ ⁱᵈᵉᵒˢ Ʈⁱᵏᵗᵒᵏ 📺⃤ ✨┃
+https://chat.whatsapp.com/GtXAWDlphvw3XHcb7scmdw
 
+▢ ┃ 💜 😍 丂ㄒ🍄匚Ҝ乇尺丂 🌺 𒂶 ✨ ᵘⁿⁱᵛᵉʳˢⁱᵗʸ 🎭 🧡 ┃
+https://chat.whatsapp.com/JiqP6NTNWbw84kmJKO5U8x
+
+▢ ┃🎬⃤🍿 Ꭾ𝜩Ꮭ༏ᏨปᏝ𐤠Ⲋ  𒂶  Ⲋ𝜩☈༏𝜩Ⲋ 📽️⃤🖥️ ┃
+https://chat.whatsapp.com/EST7ZYKGBvQ2v4ZSTHIGSd
+
+▢ ┃✨💓⃤Ɯ𐤠꤈꤈Ꭾ𐤠Ꭾ𝜩☈'Ⲋ  𒂶  Ꝼ𐔖ꤚⱰ𐔖Ⲋ  Ɗᵉ  Ꭾᵃⁿᵗᵃˡˡᵃˢ 🌌⃤✨┃
+https://chat.whatsapp.com/FTfdyf392efG8Zazl6zrMC
+ 
+▢ ┃🖤 😈 丂ㄒ🩸匚Ҝ乇尺丂 🥵 𒂶 💦 𐤠ᵈᵘˡᵗᵒˢ ➊➑⁺ 𒀭 🤤 🤍 ┃
+https://chat.whatsapp.com/Bkh5CjKhZit5XnbK0DaRRi
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ Todos los Grupos
- https://instabio.cc/fg98ff
-
-▢ *Telegram*
-• https://t.me/fgawgp
- ▢ *PayPal*
-• https://paypal.me/fg98f
-▢ *YouTube*
-• https://www.youtube.com/fg98f`)
 
 }
 handler.help = ['support']
 handler.tags = ['main']
-handler.command = ['grupos', 'groups', 'support'] 
+handler.command = ['gplk', 'lowkoo', 'groups'] 
 
 export default handler
