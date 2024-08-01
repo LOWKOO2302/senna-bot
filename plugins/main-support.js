4c5c5561e59c70e4a0d67db16613050b1ea56c92
LOWKOO2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-08-01 03:04:32 -0400

    Update main-support.js

--- plugins/main-support.js
+++ plugins/main-support.js
@@ -6,28 +6,28 @@ m.reply(`
 
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
 ▢ ┃ 𒀯  Ⲙ  ⵡ  Ⲡ  Ɗ  Ⲟ  𒂶 𐓈𝛯  𐤠Ƿ𝙊☈Ʈ𝛯Ⲋ   ➊➑⁺ 💬  𒀯 ┃
-https://chat.whatsapp.com/Ke1Aghdz0QVII6kyXSQv7E 
+ https://chat.whatsapp.com/Ke1Aghdz0QVII6kyXSQv7E 
 
 ▢ ┃𒀭⛓️𝜩ⲚᏝ𐤠Ꮸ𝜩Ⲋ 𒂶 Ꭾ𝜩Ꮭ༏ᏨปᏝ𐤠Ⲋ ⁿᵉᵗᶠˡⁱˣ ᵒⁿˡⁱⁿᵉ 🍿 𒀭┃
-https://chat.whatsapp.com/HvzT09WBkuG9DIa7e4zMuE
+ https://chat.whatsapp.com/HvzT09WBkuG9DIa7e4zMuE
 
 ▢ ┃🖲️⃢🎮 ꤈𝜩Ƭ'𝚂 Ƿ꤈𐤠ⲩ 𒂶 ᒍꪙ𝜩ᏵⲞⲊ 𐤠ᴾᴷˢ ꖾᴬᶜᴷˢ 📲⃢🍿┃
-https://chat.whatsapp.com/Hq2bBBAg3bpBDo1Zrpfw3Y
+ https://chat.whatsapp.com/Hq2bBBAg3bpBDo1Zrpfw3Y
 
 ▢ ┃✨ 🎧⃤ ⏪⏸⏩ Ⲙ 𞤋 Ꚃ ༏ ꓚ 𐤠 Ꚃ  𒂶  ꪜ ⁱᵈᵉᵒˢ Ʈⁱᵏᵗᵒᵏ 📺⃤ ✨┃
-https://chat.whatsapp.com/GtXAWDlphvw3XHcb7scmdw
+ https://chat.whatsapp.com/GtXAWDlphvw3XHcb7scmdw
 
 ▢ ┃ 💜 😍 丂ㄒ🍄匚Ҝ乇尺丂 🌺 𒂶 ✨ ᵘⁿⁱᵛᵉʳˢⁱᵗʸ 🎭 🧡 ┃
-https://chat.whatsapp.com/JiqP6NTNWbw84kmJKO5U8x
+ https://chat.whatsapp.com/JiqP6NTNWbw84kmJKO5U8x
 
 ▢ ┃🎬⃤🍿 Ꭾ𝜩Ꮭ༏ᏨปᏝ𐤠Ⲋ  𒂶  Ⲋ𝜩☈༏𝜩Ⲋ 📽️⃤🖥️ ┃
-https://chat.whatsapp.com/EST7ZYKGBvQ2v4ZSTHIGSd
+ https://chat.whatsapp.com/EST7ZYKGBvQ2v4ZSTHIGSd
 
 ▢ ┃✨💓⃤Ɯ𐤠꤈꤈Ꭾ𐤠Ꭾ𝜩☈'Ⲋ  𒂶  Ꝼ𐔖ꤚⱰ𐔖Ⲋ  Ɗᵉ  Ꭾᵃⁿᵗᵃˡˡᵃˢ 🌌⃤✨┃
-https://chat.whatsapp.com/FTfdyf392efG8Zazl6zrMC
+ https://chat.whatsapp.com/FTfdyf392efG8Zazl6zrMC
  
 ▢ ┃🖤 😈 丂ㄒ🩸匚Ҝ乇尺丂 🥵 𒂶 💦 𐤠ᵈᵘˡᵗᵒˢ ➊➑⁺ 𒀭 🤤 🤍 ┃
-https://chat.whatsapp.com/Bkh5CjKhZit5XnbK0DaRRi
+ https://chat.whatsapp.com/Bkh5CjKhZit5XnbK0DaRRi
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
 
 }
